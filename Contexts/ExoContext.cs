aabf0da2a262bc76bfc83a21c89b5b6d92cfffa9
Nayan <nayan.estudos@gmail.com>
2025-10-01 01:30:55 -0300

    first commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,26 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext: DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext (DbContextOptions<ExoContext> options): base(options)
+        {
+        }
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                // string de conexão
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi; Trusted_Connection=True;");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
